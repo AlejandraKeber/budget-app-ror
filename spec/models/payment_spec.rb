1ab4452bb672809c33df26e7d029761c5ef49a62
AlejandraKeber <alejandrakeber@gmail.com>
2023-07-05 16:09:18 -0500

    Add model specs

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  let(:user) { User.create(name: 'Test User', email: 'test@gmail.com') }
+  let(:group) { Group.create(name: 'Test Group', icon: 'group-icon', user: user) }
+  subject { Payment.create(name: 'Test Payment', amount: 10, author_id: user.id, groups: [group]) }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be too long' do
+    subject.name = 'a' * 51
+    expect(subject).to_not be_valid
+  end
+
+  it 'amount must be a number' do
+    subject.amount = 'abc'
+    expect(subject).to_not be_valid
+  end
+
+  it 'group quantity must not be blank' do
+    subject.groups = []
+    expect(subject).to_not be_valid
+  end
+end
